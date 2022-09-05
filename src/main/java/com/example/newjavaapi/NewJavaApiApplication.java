06a338f9b411146440a4ec17db9e24fbec60e38b
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-05 13:13:49 -0700

    init commit

---
+++ src/main/java/com/example/newjavaapi/NewJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.newjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJavaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewJavaApiApplication.class, args);
+    }
+
+}
